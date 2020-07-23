8aeb4e2a7a93e43a416f6248912c7332b33e2d6d
Sebastian Scheib <stackpointerex@gmail.com>
2020-07-22 21:03:51 -0300

    otro cambio

--- src/test/java/myTests/TestSearch.java
+++ src/test/java/myTests/TestSearch.java
@@ -36,7 +36,7 @@ public class TestSearch {
 	public void searchDresses() {
 		pIndex.search("DRESSES");
 		try {
-			Thread.sleep(4000);
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
